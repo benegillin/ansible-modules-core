a829030d019ebb9178d234f163642849c11d4c29
Dag Wieers <dag@wieers.com>
2016-11-18 19:11:06 +0100

    Removing an non-existing directory complains
    The following playbook:
    
    ```yaml
    - hosts: localhost
      tasks:
      - file:
          path: /tmp/non-existing-foo-bar
          state: absent
          recurse: yes
    ```
    
    causes this error:
    
    ```
    fatal: [localhost]: FAILED! => {"changed": false, "failed": true, "msg": "recurse option requires state to be 'directory'", "path": "/tmp/non-existing-foo-bar", "state": "absent"}
    ```
    
    The included fix ensures that when recurse is added, we no longer assume
    it is a file, but accept that it is a directory.

--- files/file.py
+++ files/file.py
@@ -198,6 +198,7 @@ def main():
 
     params = module.params
     state = params['state']
+    recurse = params['recurse']
     force = params['force']
     diff_peek = params['diff_peek']
     src = params['src']
@@ -228,6 +229,8 @@ def main():
     if state is None:
         if prev_state != 'absent':
             state = prev_state
+        elif recurse:
+            state = 'directory'
         else:
             state = 'file'
 
@@ -253,7 +256,6 @@ def main():
             b_path = to_bytes(path, errors='surrogate_or_strict')
 
     # make sure the target path is a directory when we're doing a recursive operation
-    recurse = params['recurse']
     if recurse and state != 'directory':
         module.fail_json(path=path, msg="recurse option requires state to be 'directory'")
 
